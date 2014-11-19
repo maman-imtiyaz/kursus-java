bc800383ba5c018cb4eb8a46c6034c46805e1704
unknown <aaa@yahoo.com>
2014-11-19 21:33:04 +0700

    coba

--- KonversiTemperature.java
+++ KonversiTemperature.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -27,6 +27,7 @@ public class KonversiTemperature {
         c= 45;
         f= 113;
         r= 36;
+	ijfkjcnkdsjoe
        
         //konversi c --> f
         System.out.println("Program Konversi C ke F");
